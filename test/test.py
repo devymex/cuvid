b00b92725b2df8c6d650ec8ed8e9ff603cf1b076
Devymex <wangyumeng02@megvii.com>
2020-11-16 16:45:33 +0800

    test.py updated

--- test/test.py
+++ test/test.py
@@ -5,10 +5,12 @@ import prand, cv2
 dec = prand.Prand('rtsp://10.201.105.94/user=admin&password=&channel=1&stream=0.sdp', 1)
 dec.start()
 while True:
-	frame_id, img = dec.get_frame()
+	frame_id, img1, jpg = dec.get_frame()
 	if frame_id > 0:
-		img = cv2.resize(img, (960, 540))
-		cv2.imshow('img', img)
+		img1 = cv2.resize(img1, (960, 540))
+		img2 = cv2.imdecode(jpg, cv2.IMREAD_COLOR)
+		cv2.imshow("Downloaded from GPU", img1)
+		cv2.imshow("Decode From JPEG", img2)
 		key = cv2.waitKey(0) & 0xFF
 		if key == 27:
 			break
