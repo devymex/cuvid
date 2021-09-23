f1f38d60af779724cca1e6172b372c16a08a87fe
Devymex <wangyumeng02@megvii.com>
2021-09-23 07:58:27 +0000

    bug fixed

--- python/__init__.py
+++ python/__init__.py
@@ -17,5 +17,5 @@ class Cuvid:
         return cuvid.cuvid_errcode(self._cuvid)
 
     def read(self):
-        frame_id, img = cuvid.cuvid_read(self._cuvid, False)
+        frame_id, img = cuvid.cuvid_read(self._cuvid)
         return (frame_id, img)
